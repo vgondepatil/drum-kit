5467c6c65a194364b40366848282c4239f5b632b
Varad Gondepatil <vgondepatil@gmail.com>
2023-03-29 01:11:52 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,77 @@
+
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+//Detecting buttons press   
+
+for(var i =0;i<numberOfDrumButtons;i++)
+{
+document.querySelectorAll("button")[i].addEventListener('click',function () {
+
+
+    var buttonInnerHTML = this.innerHTML
+    makeSound(buttonInnerHTML)
+    buttonAnimation(buttonInnerHTML)
+  
+});
+
+
+// Detectin Key presses
+
+}
+addEventListener("keypress",function(event){
+        makeSound(event.key)
+        buttonAnimation(event.key)
+});
+
+
+function makeSound(key) {
+    switch (key) {
+        case "w":
+            var audio = new Audio("sounds/tom-1.mp3")
+            audio.play();
+            break;
+        
+        case "a":
+            var audio = new Audio("sounds/tom-2.mp3")
+            audio.play();
+            break;
+        
+        case "s":
+            var audio = new Audio("sounds/tom-3.mp3")
+            audio.play();
+            break;
+
+        case "d":
+            var audio = new Audio("sounds/tom-3.mp3")
+            audio.play();
+            break;
+
+        case "j":
+            var audio = new Audio("sounds/snare.mp3")
+            audio.play();
+            break;
+
+        case "k":
+            var audio = new Audio("sounds/crash.mp3")
+            audio.play();
+            break;
+        case "l":
+            var audio = new Audio("sounds/kick-bass.mp3")
+            audio.play();
+            break;
+        default:
+            console.log(buttonInnerHTML);
+    }
+}
+
+function buttonAnimation(currentKey) {
+    var activeButton = document.querySelector("."+currentKey);
+    activeButton.classList.add("pressed");
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    },120)
+    
+}
+
+// var audio = new Audio("melodyloops-preview-rhythmic-indian-language-2m30s.mp3")
+// audio.play()
